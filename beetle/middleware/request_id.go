42dc0de12f74c5a42acfaab28d7dff202a2cc02e
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2025-05-01 20:56:52 +0100

    chore; pusher

--- beetle/middleware/request_id.go
+++ beetle/middleware/request_id.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 )
@@ -12,9 +11,6 @@ func (m *Middleware) RequestID() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		reqID := uuid.New().String()
 
-		fmt.Println(reqID)
-
-		// Set the request ID in context and header
 		c.Set(RequestIDKey, reqID)
 		c.Writer.Header().Set(RequestIDKey, reqID)
 
